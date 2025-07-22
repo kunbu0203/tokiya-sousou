9a8a7c6cdd3a281f96b8dbeb3f83c2902a0ee8bb
pon <kunbu0203@gmail.com>
2025-07-22 20:35:04 +0800

    initial commit

---
+++ src/assets/js/main.js
@@ -0,0 +1,15 @@
+(function (targetWidth) {
+    var deviceWidth = screen.width;
+    var ratio = deviceWidth / targetWidth;
+    var viewport = document.querySelector('meta[name="viewport"]');
+    if (ratio < 1) {
+        viewport.setAttribute('content', 'width=device-width, initial-scale=' + ratio + ', minimum-scale=' + ratio + ', maximum-scale=' + ratio + ', user-scalable=yes');
+    }
+})(390);
+
+(function () {
+    $(window).on('resize.vh', function () {
+        var vh = window.innerHeight * 0.01;
+        $('html').css('--vh', vh + 'px');
+    }).trigger('resize.vh');
+})();
